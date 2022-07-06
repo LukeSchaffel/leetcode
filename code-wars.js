dcfd6b3ab537ba2dede7c268146d5ace860f1a55
LukeSchaffel <lukeschaffel@gmail.com>
2022-07-06 12:00:49 -0400

    checks for zeros

--- code-wars.js
+++ code-wars.js
@@ -75,3 +75,52 @@
 // https://www.codewars.com/kata/52c4dd683bfd3b434c000292/train/javascript
 // catching car mileage number
 
+
+
+const isInteresting = (number, awesomePhrases) => {
+  if (number < 100) return 0
+
+  // check if number is in awesomePhrases
+  if (awesomePhrases.includes(number)) return 2
+
+  //check if number is approaching awesomePhrases
+  if(awesomePhrases.length > 0){
+    for (let i = 0; i < awesomePhrases.length; i++) {
+      const element = awesomePhrases[i];
+      if (element - 1 === number || element -2 === number) {
+        return 1
+      }
+    }
+  }
+
+  const arrayNumber = number.toString().split('').map((num)=>parseInt(num))
+  //check if digit followed by all zeros
+  // const allZeros = checkForZeros(arrayNumber)
+  // if (allZeros) return 2
+  const zeros = checkForZeros(number)
+  console.log(zeros);
+
+
+
+ return 0
+}
+
+const checkForZeros = (number) => {
+  const arrayNumber = number.toString().split('').map((num)=>parseInt(num))
+  arrayNumber.shift()
+  let checker = '1'
+  for (let i = 0; i < arrayNumber.length; i++) {
+    checker += '0'
+  }
+  if (number % parseInt(checker) === 0) {
+    return 2
+  }
+  if ((number + 1) % parseInt(checker) === 0 || (number + 2) % parseInt(checker) === 0) {
+    return 1
+  }
+}
+
+
+
+
+console.log(isInteresting(1001, []));

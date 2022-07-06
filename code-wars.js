18dff3fac9c7390248fa51462186a53858626509
LukeSchaffel <lukeschaffel@gmail.com>
2022-07-06 10:26:02 -0400

    pick peaks

--- code-wars.js
+++ code-wars.js
@@ -26,13 +26,12 @@
 //   return answers
 // }
 function pickPeaks(arr) {
-  if (arr.length < 3) return
+  const answers = { pos: [], peaks: [] }
+  if (arr.length < 3) return answers
 
   let i = 0
   let j = 1
   let k = 2
-
-  const answers = { pos: [], peaks: [] }
 
   while (i < arr.length) {
     let left = arr[i]
@@ -42,28 +41,26 @@ function pickPeaks(arr) {
       answers.pos.push(j)
       answers.peaks.push(center)
     }
-    if (center === right && center === left) {
-      i--
+
+    if (center === right && center > left) {
       while (arr[j] === arr[k]) {
         k++
       }
       if (arr[j] > arr[i] && arr[j] > arr[k]) {
-        j--
         answers.pos.push(j)
         answers.peaks.push(arr[j])
       }
-      j = k-1
-      i = k-2
+      j = k - 1
+      i = k - 2
     }
-   
-    
+
     i++
     j++
     k++
-
   }
-  console.log(answers);
+
   return answers
 }
-                                   // i  j        k
-pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1])
+
+pickPeaks([1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3])
+

2b961952223a83fba29d60a0e31dd36b0779a591
LukeSchaffel <lukeschaffel@gmail.com>
2022-07-10 10:58:09 -0400

    isPrime

--- code-wars.js
+++ code-wars.js
@@ -77,132 +77,176 @@
 
 
 
-const isInteresting = (number, awesomePhrases) => {
-  if (number === 98 || number === 99) return 1
-  if (number < 100) return 0
-  const arrayNumber = number.toString().split('').map((num) => parseInt(num))
+// const isInteresting = (number, awesomePhrases) => {
+//   if (number === 98 || number === 99) return 1
+//   if (number < 100) return 0
+//   const arrayNumber = number.toString().split('').map((num) => parseInt(num))
 
-  // check if number is in awesomePhrases
-  if (awesomePhrases.includes(number)) return 2
+//   // check if number is in awesomePhrases
+//   if (awesomePhrases.includes(number)) return 2
 
-  //check if number is approaching awesomePhrases
-  if (awesomePhrases.length > 0) {
-    for (let i = 0; i < awesomePhrases.length; i++) {
-      const element = awesomePhrases[i];
-      if (element - 1 === number || element - 2 === number) {
-        return 1
+//   //check if number is approaching awesomePhrases
+//   if (awesomePhrases.length > 0) {
+//     for (let i = 0; i < awesomePhrases.length; i++) {
+//       const element = awesomePhrases[i];
+//       if (element - 1 === number || element - 2 === number) {
+//         return 1
+//       }
+//     }
+//   }
+
+//   if(checkForZeros(number)) return 2
+//   if(checkForSameNumber(number)) return 2
+//   if(checkIncrementing(number)) return 2
+//   if(checkDecrementing(number)) return 2
+//   if(checkPalendrome(number)) return 2
+
+
+//   if(checkForZeros(number + 1)) return 1
+//   if(checkForZeros(number + 2)) return 1
+
+//   if(checkForSameNumber(number + 1)) return 1
+//   if(checkForSameNumber(number + 2)) return 1
+
+//   if(checkIncrementing(number + 1)) return 1
+//   if(checkIncrementing(number + 2)) return 1
+
+//   if(checkDecrementing(number + 1)) return 1
+//   if(checkDecrementing(number + 2)) return 1
+
+//   if(checkPalendrome(number + 1)) return 1
+//   if(checkPalendrome(number + 2)) return 1
+
+//   return 0
+// }
+
+// const checkForZeros = (number) => {
+//   const arrayNumber = number.toString().split('').map((num) => parseInt(num))
+//   let checker = '1'
+//   for (let i = 1; i < arrayNumber.length; i++) {
+//     checker += '0'
+//   }
+//   if (number % parseInt(checker) === 0) {
+//     return 2
+//   }
+// }
+
+// const checkForSameNumber = (number) => {
+//   const first = number % 10;
+//   while (number) {
+//     if (number % 10 !== first) return false;
+//     number = Math.floor(number / 10);
+//   }
+//   return true
+// }
+
+// const checkIncrementing = (number) => {
+//   const arrayNumber = number.toString().split('').map((num) => parseInt(num))
+//   for (let i = 0; i < arrayNumber.length; i++) {
+//     let num = arrayNumber[i];
+//     if(num === 0)arrayNumber[i] = 10
+
+//   }
+//   console.log(arrayNumber);
+
+//   let pointer = arrayNumber[0]
+//   for (let i = 1; i < arrayNumber.length; i++) {
+//     let current = arrayNumber[i];
+//     if (current !== (pointer + 1)){
+//       return false
+//     }
+//     pointer ++
+//   }
+//   return true
+// }
+
+// const checkDecrementing = (number) => {
+//   const arrayNumber = number.toString().split('').map((num) => parseInt(num))
+//   let pointer = arrayNumber[0]
+//   for (let i = 1; i < arrayNumber.length; i++) {
+//     const current = arrayNumber[i];
+//     if (current !== (pointer - 1)){
+//       return false
+//     }
+//     pointer --
+//   }
+//   return true
+// }
+
+// const checkPalendrome = (number) => {
+//   const arrayNumber = number.toString().split('').map((num) => parseInt(num))
+//   const reversed = [...arrayNumber].reverse()
+
+//   for (let i = 0; i < arrayNumber.length; i++) {
+//     if (arrayNumber[i] !== reversed[i]) return false
+
+//   }
+
+//   return true
+// }
+
+
+
+// console.log(isInteresting(7899, []));
+
+//https://www.codewars.com/kata/55983863da40caa2c900004e/train/javascript
+
+//next bigger number with the same digits
+
+
+// const nextBigger = (n) => {
+//   const array = n.toString().split('').map(digit => parseInt(digit))
+
+//   for (let i = 0; i < array.length; i++) {
+//     const num = array[i];
+//     for (let j = 1; j < array.length; j++){
+//       console.log(num + array[j]);
+
+//     }
+
+//   }
+// }
+
+// console.log(nextBigger(12))
+
+
+// gap in primes
+// https://www.codewars.com/kata/561e9c843a2ef5a40c0000a4/train/javascript
+
+const gap = (g, m, n) => {
+  let currentGap = 0
+  let prev
+
+  for (let i = m; i <= n; i++) {
+    const current = i;
+    if (isPrime(current)) {
+      if (!prev) {
+        currentGap = 0
+        prev = current
       }
+      if (currentGap === g) {
+        console.log(prev, current, currentGap)
+        return [ prev, current ]
+      }
+      currentGap = 0
+      prev = current
     }
+    currentGap ++
   }
-
-  // check if all trailing zeros
-  // const zeros = checkForZeros(arrayNumber, number)
-  // if (zeros === 1 || zeros === 2 ) return zeros
-  if(checkForZeros(number)) return 2
-  if(checkForSameNumber(number)) return 2
-  if(checkIncrementing(number)) return 2
-  if(checkDecrementing(number)) return 2
-  if(checkPalendrome(number)) return 2
-
-
-
-
-
-
-  if(checkForZeros(number + 1)) return 1
-  if(checkForZeros(number + 2)) return 1
-
-  //check for repeating numbers
-  
-  if(checkForSameNumber(number + 1)) return 1
-  if(checkForSameNumber(number + 2)) return 1
-
-  //check for incrementing
-  
-  if(checkIncrementing(number + 1)) return 1
-  if(checkIncrementing(number + 2)) return 1
-
-  //check for decrementing
-  
-  if(checkDecrementing(number + 1)) return 1
-  if(checkDecrementing(number + 2)) return 1
-
-  //check palendrom
-  
-  if(checkPalendrome(number + 1)) return 1
-  if(checkPalendrome(number + 2)) return 1
-  
-
-
-
-  return 0
+  return null
 }
 
-const checkForZeros = (number) => {
-  const arrayNumber = number.toString().split('').map((num) => parseInt(num))
-  let checker = '1'
-  for (let i = 1; i < arrayNumber.length; i++) {
-    checker += '0'
-  }
-  if (number % parseInt(checker) === 0) {
-    return 2
-  }
-}
-
-const checkForSameNumber = (number) => {
-  const first = number % 10;
-  while (number) {
-    if (number % 10 !== first) return false;
-    number = Math.floor(number / 10);
-  }
-  return true
-}
-
-const checkIncrementing = (number) => {
-  const arrayNumber = number.toString().split('').map((num) => parseInt(num))
-  for (let i = 0; i < arrayNumber.length; i++) {
-    let num = arrayNumber[i];
-    if(num === 0)arrayNumber[i] = 10
-    
-  }
-  console.log(arrayNumber);
-
-  let pointer = arrayNumber[0]
-  for (let i = 1; i < arrayNumber.length; i++) {
-    let current = arrayNumber[i];
-    if (current !== (pointer + 1)){
+const isPrime = (number) => {
+  if (number === 1) return false
+  if (number === 2) return true
+  for (let i = 2; i < number - 1; i++) {
+    const current = i
+    if (number % current === 0) {
       return false
     }
-    pointer ++
   }
   return true
 }
 
-const checkDecrementing = (number) => {
-  const arrayNumber = number.toString().split('').map((num) => parseInt(num))
-  let pointer = arrayNumber[0]
-  for (let i = 1; i < arrayNumber.length; i++) {
-    const current = arrayNumber[i];
-    if (current !== (pointer - 1)){
-      return false
-    }
-    pointer --
-  }
-  return true
-}
 
-const checkPalendrome = (number) => {
-  const arrayNumber = number.toString().split('').map((num) => parseInt(num))
-  const reversed = [...arrayNumber].reverse()
-
-  for (let i = 0; i < arrayNumber.length; i++) {
-    if (arrayNumber[i] !== reversed[i]) return false
-    
-  }
-
-  return true
-}
-
-
-
-console.log(isInteresting(7899, []));
+console.log(gap(11, 30000, 100000))

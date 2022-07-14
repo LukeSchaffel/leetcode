cd0b34071ca5fa76909d0d36ce50d50bbcbe49ff
LukeSchaffel <lukeschaffel@gmail.com>
2022-07-14 13:06:16 -0400

    trash

--- code-wars.js
+++ code-wars.js
@@ -281,3 +281,167 @@ function validate(password) {
   return /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(password);
 }
 
+// sudoku https://www.codewars.com/kata/540afbe2dc9f615d5e000425/train/javascript
+
+var Sudoku = function (data) {
+  //   Private methods
+  // -------------------------
+
+
+
+  //   Public methods
+  // -------------------------
+  return {
+    isValid: function () {
+      // YOUR SOLUTION
+      if (!validRows(data)) return false
+      if (!validBlocks(data)) return false
+      if (!validColumns(data)) return false
+
+      return true;
+    }
+  };
+};
+
+const validRows = (data) => {
+
+  for (let i = 0; i < data.length; i++) {
+    const row = data[i];
+    const rowSet = new Set(row.map((number, j) => {
+      return number
+    }))
+    if (rowSet.size !== row.length) {
+      return false
+    }
+
+  }
+  return true
+}
+
+const validBlocks = (data) => {
+
+  const blocks = []
+
+  blocks.push(new Set([
+    data[0][0], data[0][1], data[0][2],
+    data[1][0], data[1][1], data[1][2],
+    data[2][0], data[2][1], data[2][2],
+  ]))
+
+  blocks.push(new Set([
+    data[0][3], data[0][4], data[0][5],
+    data[1][3], data[1][4], data[1][5],
+    data[2][3], data[2][4], data[2][5],
+  ]))
+  blocks.push(new Set([
+    data[0][6], data[0][7], data[0][8],
+    data[1][6], data[1][7], data[1][8],
+    data[2][6], data[2][7], data[2][8],
+  ]))
+
+  blocks.push(new Set([
+    data[3][0], data[3][1], data[3][2],
+    data[4][0], data[4][1], data[4][2],
+    data[5][0], data[5][1], data[5][2],
+  ]))
+
+  blocks.push(new Set([
+    data[3][3], data[3][4], data[3][5],
+    data[4][3], data[4][4], data[4][5],
+    data[5][3], data[5][4], data[5][5],
+  ]))
+  blocks.push(new Set([
+    data[3][6], data[3][7], data[3][8],
+    data[4][6], data[4][7], data[4][8],
+    data[5][6], data[5][7], data[5][8],
+  ]))
+  //bottom row
+  blocks.push(new Set([
+    data[6][0], data[6][1], data[6][2],
+    data[7][0], data[7][1], data[7][2],
+    data[8][0], data[8][1], data[8][2],
+  ]))
+  blocks.push(new Set([
+    data[6][3], data[6][4], data[6][5],
+    data[7][3], data[7][4], data[7][5],
+    data[8][3], data[8][4], data[8][5],
+  ]))
+  blocks.push(new Set([
+    data[6][6], data[6][7], data[6][8],
+    data[7][6], data[7][7], data[7][8],
+    data[8][6], data[8][7], data[8][8],
+  ]))
+
+  for (let i = 0; i < blocks.length; i++) {
+    const element = blocks[i];
+    if (element.size !== 9) {
+      return false
+    }
+    
+  }
+
+  return true
+  
+  
+
+  
+  
+ 
+  console.log(blocks);
+
+
+}
+
+const validColumns = (data) => {
+  const columnZero = new Set()
+  const columnOne = new Set()
+  const columnTwo = new Set()
+  const columnThree = new Set()
+  const columnFour = new Set()
+  const columnFive = new Set()
+  const columnSix = new Set()
+  const columnSeven = new Set()
+  const columnEight = new Set()
+
+  for (let i = 0; i < data.length; i++) {
+    const column = data[i];
+    columnZero.add(column[0])
+    columnOne.add(column[1])
+    columnTwo.add(column[2])
+    columnThree.add(column[3])
+    columnFour.add(column[4])
+    columnFive.add(column[5])
+    columnSix.add(column[6])
+    columnSeven.add(column[7])
+    columnEight.add(column[8])
+  }
+
+  const columns = [columnZero, columnOne, columnTwo, columnThree, columnFour, columnFive, columnSix, columnSeven,columnEight  ]
+
+  for (let i = 0; i < columns.length; i++) {
+    const element = columns[i];
+    if (element.size !== 9) {
+      return false
+    }
+    
+  }
+
+  return true
+    
+  
+}
+
+console.log(
+validColumns([
+  [7, 8, 4, 1, 5, 9, 3, 2, 6],
+  [5, 3, 9, 6, 7, 2, 8, 4, 1],
+  [6, 1, 2, 4, 3, 8, 7, 5, 9],
+
+  [9, 2, 8, 7, 1, 5, 4, 6, 3],
+  [3, 5, 7, 8, 4, 6, 1, 9, 2],
+  [4, 6, 1, 9, 2, 3, 5, 8, 7],
+
+  [8, 7, 6, 3, 9, 4, 2, 1, 5],
+  [2, 4, 3, 5, 6, 1, 9, 7, 8],
+  [1, 9, 5, 2, 8, 7, 6, 3, 4]
+]))
